0214eb4f330787b863155e3561a6cc635db38334
sametcandan14 <c._samet@hotmail.com>
2023-03-21 23:51:59 +0300

    images and product added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product-list/product/product.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, ProductListComponent],
+  declarations: [AppComponent, NavbarComponent, ProductListComponent, ProductComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/product-list/product/product.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent {}
